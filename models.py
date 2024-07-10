c35ed85e921f624586ba49d0deb7a760c7b8ef0b
Franklin Kong <franklinkong981@gmail.com>
2024-07-10 15:55:33 -0700

    Edited schema and set up restaurant, menu item models

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 """SQLAlchemy models for the Vote4Food app."""
 
-from datetime import datetime
+from datetime import datetime, timezone
 
 from flask_bcrypt import Bcrypt #For user signup/login password hashing
 from flask_sqlalchemy import SQLAlchemy
@@ -10,15 +10,17 @@ db = SQLAlchemy()
 
 class User(db.Model):
     """User in the app. A user can create an account by signing up, then logging in.
-    User will sign up with a unique email, unique username, password, and optional profile picture."""
+    User will sign up with a first name, last name, unique email address, password, and optional profile picture."""
 
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
     email = db.Column(db.Text, nullable=False, unique=True)
-    username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
     user_image_url = db.Column(db.Text, default="vote4food_default")
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
     
     # Once logged in, users can set their address which will be converted into longitude and latitude for restaurant lookup.
     location_lat = db.Column(db.Float)
@@ -27,15 +29,28 @@ class User(db.Model):
 class Restaurant(db.Model):
     """Restaurant location, NOT restaurant chain. Each location of a restaurant will be a different instance of the Restaurant model.
     This is for restaurant location lookup. Contains some restaurant location data taken from the API so that locations that have
-    been upvoted/downvoted, reviewed, and/or favorited by the user can be accessed quickly."""
+    been reviewed and/or favorited by the user can be accessed quickly."""
 
     __tablename__ = "restaurats"
 
+    id = db.Column(db.Text, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    address = db.Column(db.Text)
+    description = db.Column(db.Text)
+    cuisines = db.Column(db.Text)
+    store_photo_url = db.Column(db.Text)
+    logo_photo_url = db.Column(db.Text, default="vote4food_default")
+
 class Item(db.Model):
-    """Restaurant menu item. Contains some data for a menu item taken from the API so that items that have been upvoted/downvoted,
-    reviewed, and/or favorited by the user can be accessed quickly."""
+    """Restaurant menu item. Contains some data for a menu item taken from the API so that items that have been reviewed 
+    and/or favorited by the user can be accessed quickly."""
 
     __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False)
+    restaurant_chain = db.Column(db.Text)
+    image_url =db.Column(db.Text, default="vote4food_default")
 
 
 
