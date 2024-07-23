d70c402df045776ec24997c570c799e1fad82753
Franklin Kong <franklinkong981@gmail.com>
2024-07-23 15:05:04 -0700

    Defined form for creating an account

--- forms.py
+++ forms.py
@@ -1,3 +1,16 @@
+"""This is the file where the different forms and their different fields/constraints are specified. This application uses WTForms
+for increased security."""
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField, SelectField, IntegerField
 from wtforms.validators import DataRequired, Email, Length, Optional
+
+class SignUpForm(FlaskForm):
+    """Defines the form for user signup when creating a new account. Users will input their first name, last name, email, password,
+    and optional profile image URL. The email must be unique and the password must be at least 8 characters long."""
+
+    first_name = StringField('First Name:', validators=[DataRequired(message="You must provide a first name!")])
+    last_name = StringField('Last Name:', validators=[DataRequired(message="You must provide a last name!")])
+    email = StringField('Email Address:', validators=[DataRequired(message="You must provide an email!"), Email("You must provide a valid email address!")])
+    user_image_url = StringField('Profile Image URL (optional):', validators=[Optional()])
+    password = password = PasswordField('Password', validators=[DataRequired(), Length(min=8,message="Your password must be at least 8 characters long!")])
