8090b1628060a0632c7b38699c5748c1639d2f1d
Franklin Kong <franklinkong981@gmail.com>
2024-08-13 22:35:56 -0700

    Fixed HTML menu item bug

--- app.py
+++ app.py
@@ -641,7 +641,7 @@ def create_app(db_name, testing=False):
     # Routes relevant to menu item pages, including adding/removing a menu item from the user's favorites and creating, editing, 
     # and deleting a review for a menu item.
     
-    @app.route("/items/<int: item_id>")
+    @app.route("/items/<int:item_id>")
     def display_item_page(item_id):
         """Displays detailed information of a particular menu item. The page should have some information about the item. 
         There should also be buttons/links for the user to add/remove the item from their favorites and for a user to create,
