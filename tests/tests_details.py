4bf38866e68fa17dbe7eec052403a1abe73d8187
Franklin Kong <franklinkong981@gmail.com>
2024-08-21 00:36:32 -0700

    Added tests for restaurant details page

--- tests/tests_details.py
+++ tests/tests_details.py
@@ -53,8 +53,49 @@ class ProfileTestCase(TestCase):
       user_image_url = "www.testurl1.com",
       password = "PASSWORD1"
     )
+    db.session.add(user_ted)
 
-    
+    # Create sample restaurant
+    arbys = Restaurant(
+      id = "restaurant1",
+      name = "Arby's",
+      address = "California",
+      cuisines = "Sandwiches, fast food, burgers",
+      description = "Good fast food place that sells burgers and sandwiches",
+      phone = "1-(858)-857-9476",
+      photo_url = "www.arbysimage.jpg",
+      latitude = 123.456,
+      longitude = 654.321
+    )
+
+    mcdonalds = Restaurant(
+      id = "restaurant2",
+      name = "McDonald's",
+      address = "San Diego, California, 92129",
+      cuisines = "Fast food, burgers, ice cream",
+      description = "Famous fast food place that sells burgers, fries, and has a value menu.",
+      phone = "1-(858)-947-3866",
+      photo_url = "www.mcdonaldsimage.jpg",
+      latitude = 578.398,
+      longitude = 857.397,
+      sunday_hours = "7:00AM-8:00PM",
+      monday_hours = "7:00AM-5:00PM",
+      tuesday_hours = "7:00AM-5:00PM",
+      wednesday_hours = "7:00AM-5:00PM",
+      thursday_hours = "7:00AM-5:00PM",
+      friday_hours = "7:00AM-5:00PM",
+      saturday_hours = "7:00AM-8:00PM"
+    )
+    db.session.add_all([arbys, mcdonalds])
+
+    # Create sample menu item
+
+    big_mac = Item(
+      title = "Big Mac",
+      restaurant_chain = "McDonald's",
+      image_url = "www.mcdonalds.com/big_mac_image.jpg"
+    )
+    db.session.add(big_mac)
 
     db.session.commit()
   
@@ -62,3 +103,78 @@ class ProfileTestCase(TestCase):
     """After each test, revert table back to original state."""
 
     db.session.rollback()
+
+  """There is one user: user_ted. There are 2 restaurants: McDonald's and Arby's. There is one menu item: Big Mac from McDonald's."""
+
+  # Tests on restaurant details page
+
+  def test_logged_out_restaurant_details_page(self):
+    """When a logged out user tries to access a restaurant details page, are they redirected back to the homepage?"""
+
+    arbys = Restaurant.query.filter(Restaurant.name == "Arby's").first()
+
+    with self.client as c:
+      resp = c.get(f"/restaurants/{arbys.id}")
+
+      self.assertEqual(resp.status_code, 302)
+      self.assertEqual(resp.location, "/")
+  
+  def test_restaurant_details(self):
+    """When a logged in user accesses a restaurant's details page, do they see the appropriate information?"""
+
+    arbys = Restaurant.query.filter(Restaurant.name == "Arby's").first()
+    user_ted = User.query.filter(User.first_name == 'Ted').first()
+
+    with self.client as c:
+      # simulate user_ted logging in
+      with c.session_transaction() as sess:
+        sess[CURRENT_USER_KEY] = user_ted.id
+      
+      resp = c.get(f"/restaurants/{arbys.id}")
+      html = resp.get_data(as_text=True)
+
+      self.assertEqual(resp.status_code, 200)
+      self.assertIn('Add to Favorites <i class="fa-regular fa-star"></i>', html)
+      self.assertIn('<h4 class="display-4">Restaurant Information</h4>', html)
+      self.assertIn('<th scope="col">Sandwiches, fast food, burgers</th>', html)
+      self.assertIn('<th scope="col">Good fast food place that sells burgers and sandwiches</th>', html)
+
+      # Appropriately says there are no reviews for this restaurant.
+      self.assertIn(' <p>There are no reviews currently for this restaurant.', html)
+    
+  def test_restaurant_details_with_hours(self):
+    """When a logged in user accesses a restaurant's with hours details page, are the hours listed?"""
+
+    mcdonalds = Restaurant.query.filter(Restaurant.name == "McDonald's").first()
+    user_ted = User.query.filter(User.first_name == 'Ted').first()
+
+    with self.client as c:
+      # simulate user_ted logging in
+      with c.session_transaction() as sess:
+        sess[CURRENT_USER_KEY] = user_ted.id
+      
+      resp = c.get(f"/restaurants/{mcdonalds.id}")
+      html = resp.get_data(as_text=True)
+
+      self.assertEqual(resp.status_code, 200)
+      self.assertIn('<th scope="col">7:00AM-8:00PM</th>', html)
+      self.assertIn('<th scope="col">7:00AM-5:00PM</th>', html)
+    
+  def test_invalid_restaurant_details(self):
+    """When a logged in user accesses an invalid restaurant id's details page, are they given a 404 error and redirected back to the homepage?"""
+
+    arbys = Restaurant.query.filter(Restaurant.name == "Arby's").first()
+    mcdonalds = Restaurant.query.filter(Restaurant.name == "McDonald's").first()
+    user_ted = User.query.filter(User.first_name == 'Ted').first()
+
+    with self.client as c:
+      # simulate user_ted logging in
+      with c.session_transaction() as sess:
+        sess[CURRENT_USER_KEY] = user_ted.id
+
+        resp = c.get(f"/restaurants/restaurant_invalid")
+
+        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.location, "/")
+
+  # Tests on menu item details page
